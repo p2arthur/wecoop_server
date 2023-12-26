475428a5ed49d07db5d9180a4d6715e902311b20
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-12-25 22:12:49 -0300

    (feat) add basic creator ranking analytics

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { FeedService } from 'src/feed/feed.service';
+import { PostInterface } from 'src/interfaces/PostInterface';
 
 @Injectable()
 export class AnalyticsService {
@@ -7,10 +8,39 @@ export class AnalyticsService {
 
   constructor(private postsServices: FeedService) {}
 
+  private async countPostsByCreator(postsList: PostInterface[]) {
+    const counter = {};
+
+    postsList.forEach((post) => {
+      const creatorAddress = post.creator_address;
+
+      if (counter[creatorAddress]) {
+        if (post.text.length > 3) {
+          counter[creatorAddress]++;
+        }
+      } else {
+        counter[creatorAddress] = 1;
+      }
+    });
+
+    return counter;
+  }
+
+  private async getTopCreators(allPosts: PostInterface[]) {
+    const postCounter = await this.countPostsByCreator(allPosts);
+    const sortedCreators = Object.keys(postCounter).sort(
+      (a, b) => postCounter[b] - postCounter[a],
+    );
+    return sortedCreators;
+  }
+
   public async getPostsAnalytics() {
     const allPosts = await this.postsServices.getAllPosts();
+    const counterPostByCreator = await this.countPostsByCreator(allPosts);
+    const creatorsRanking = await this.getTopCreators(allPosts);
+    console.log('counter2', counterPostByCreator);
     this.analyticsRawData = allPosts;
 
-    return this.analyticsRawData;
+    return { creatorsRanking, counterPostByCreator };
   }
 }
